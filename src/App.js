60532b33b2ec808c45ffce24a53ab44707d1f6b6
VladislavGuliy <vladgulij777@gmail.com>
2022-03-29 20:26:49 +0300

    Creating a navigation bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import MenuIcon from '@material-ui/icons/Menu';
+import { AppBar, Container, Toolbar, IconButton } from '@material-ui/core';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AppBar position='fixed'>
+      <Container fixed>
+        <Toolbar>
+          <IconButton edge='start' color='inherit' area-label='menu'>
+            <MenuIcon/>
+          </IconButton>
+        </Toolbar>
+      </Container>
+
+    </AppBar>
+  )
 }
 
 export default App;
